46b3c224904c5a5ea06c2559808b55b6fe6642dd
Nash-Aud <92648695+Nash-Aud@users.noreply.github.com>
2023-03-31 10:10:03 -0300

    Tab component ready

--- script.js
+++ script.js
@@ -2,13 +2,20 @@
 
 ///////////////////////////////////////
 // Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+//Smooth Scrolling
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Modal window
+const openModal = function(e) { 
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -17,6 +24,8 @@ const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
+btnsOpenModal.forEach(btn => btn.addEventListener("click", openModal));
 
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
@@ -28,4 +37,50 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+//Smooth Scrolling
+btnScrollTo.addEventListener("click", function(e) {
+  const s1coords = section1.getBoundingClientRect();  
+
+  //Scrolling Smooth 2
+  section1.scrollIntoView({behavior: "smooth"});
+});
+
+// //Page Navigation
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+  
+  //Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    console.log(id);
+    document.querySelector(id).scrollIntoView({
+    behavior: "smooth"})
+  }
+});
+
+//Tabbed Component
+const tabs = document.querySelectorAll(".operations__tab");
+const tabContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+//Event delegation instead of forEach
+tabContainer.addEventListener("click", function(e) {
+  const clicked = e.target.closest(".operations__tab");
+  console.log(clicked)
+
+  //Guard clause
+  if (!clicked) return;
+
+  //Remove active classes
+  tabs.forEach(t => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach(c => c.classList.remove("operations__content--active"));
+
+  //Active Tab
+  tabs.forEach(t => t.classList.remove("operations__tab--active"));
+  clicked.classList.add("operations__tab--active");
+
+  //Active content area
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add("operations__content--active");
 });
